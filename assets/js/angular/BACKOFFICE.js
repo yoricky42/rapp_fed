8acf8df95e25af2244b91439bf24c5f0fd4694b7
yoricky42 <yorickyves42@gmail.com>
2023-08-23 18:45:40 +0000

    first commit

---
+++ assets/js/angular/BACKOFFICE.js
@@ -0,0 +1,5 @@
+var app = angular.module('c2ci', ['angular.filter','socialLogin', 'ngCookies', 'ngSanitize', 'ui.bootstrap', 'pascalprecht.translate']);
+
+//---BASE_URL----//
+var BASE_URL = '//' + location.host + '/c2ci_back/public/';
+
